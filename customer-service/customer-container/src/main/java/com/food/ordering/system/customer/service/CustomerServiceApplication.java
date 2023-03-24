0414d7077c1d96c95afe627c3310b28c18298bb7
muditshukla3 <muditshukla3@gmail.com>
2023-03-24 10:45:04 +0530

    Added required dependencies in respective modules.

---
+++ customer-service/customer-container/src/main/java/com/food/ordering/system/customer/service/CustomerServiceApplication.java
@@ -0,0 +1,18 @@
+package com.food.ordering.system.customer.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "com.food.ordering.system")
+@EnableJpaRepositories(basePackages = {"com.food.ordering.system.customer.service.dataaccess",
+                                        "com.food.ordering.system.dataaccess"})
+@EntityScan(basePackages = {"com.food.ordering.system.customer.service.dataaccess",
+                            "com.food.ordering.system.dataaccess"})
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+}
